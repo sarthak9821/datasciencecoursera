933afe00f92b8ee3146a442e35b7f81e3e96cd4d
Sarthak9821 <sarthak.kawatra98@gmail.com>
2020-09-12 20:21:13 +0530

    Week 3 Programming Assignment

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,3 +1,4 @@
+# This code helps to make a matrix of whatever sorts the person wants
 makecacheMatrix <- function(x = matrix()){
         inv <- NULL
         set <- function(y) {
@@ -10,13 +11,14 @@ makecacheMatrix <- function(x = matrix()){
         list(set = set, get= get, setInverse = setInverse, getInverse = getInverse)
         
 }
-
+#this helps to find the inverse of a matrix
 cachesolve <- function(x, ...){
         inv <- x$getInverse()
         if(!is.null(inv)){
                 message("getting Cached Data")
                 return(inv)
         }
+        #if statement helps to tell whether the inverse of such a matrix was cached or not 
         mat <- x$get()
         inv <- solve(mat, ...)
         x$setInverse(inv)
